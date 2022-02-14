c7d68a4dfcb898543bcf103d43ee3bc339dd5176
times132 <times132@gmail.com>
2022-02-14 22:52:31 +0900

    vue test

--- lonefit-api/src/main/java/com/times/lonefitapi/controller/TestController.java
+++ lonefit-api/src/main/java/com/times/lonefitapi/controller/TestController.java
@@ -2,9 +2,11 @@ package com.times.lonefitapi.controller;
 
 import com.times.lonefitcore.domain.Test;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/test")
