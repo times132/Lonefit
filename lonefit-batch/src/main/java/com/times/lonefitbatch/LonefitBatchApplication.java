ec97c2ddecadca72868caad2d3c3a46ab1a8f88d
times132 <times132@gmail.com>
2022-03-09 04:03:51 +0900

    stove site jsoup post test

--- lonefit-batch/src/main/java/com/times/lonefitbatch/LonefitBatchApplication.java
+++ lonefit-batch/src/main/java/com/times/lonefitbatch/LonefitBatchApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LonefitBatchApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.profiles.default", "dev");
         SpringApplication.run(LonefitBatchApplication.class, args);
     }
 
--- lonefit-batch/src/main/java/com/times/lonefitbatch/jsoup/LoaDataService.java
+++ lonefit-batch/src/main/java/com/times/lonefitbatch/jsoup/LoaDataService.java
@@ -9,43 +9,59 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class LoaDataService {
 
     private static String LOA_MARKET_URL = "https://lostark.game.onstove.com/Market/List_v2";
-    private static String LOA_LOGIN_URL = "https://member.onstove.com/auth/login";
-    @Value("{loa.userId}")
+    private static String LOA_LOGIN_URL = "https://member.onstove.com/auth/signin";
+    @Value("${loa.userId}")
     private String userId;
-    @Value("{loa.password}")
+    @Value("${loa.password}")
     private String password;
 
     @PostConstruct
     public void getMarket() {
-        Connection conn = Jsoup.connect(LOA_LOGIN_URL);
+        Connection conn = Jsoup.connect("https://member.onstove.com/auth/login");
 
         try {
-
-            HashMap<String, String> reqData = new HashMap<>();
-            reqData.put("firstCategory", "50000");
-            reqData.put("secondCategory", "50010");
-            reqData.put("characterClass", "");
-            reqData.put("tier", "0");
-            reqData.put("grade", "99");
-            reqData.put("itemName", "명예의 파편");
-            reqData.put("pageNo", "1");
-            reqData.put("isInit", "false");
-            reqData.put("sortType", "7");
-
-            String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJleHBpcmVfdGltZSI6MTY0NjY4MjIzNjc0MiwibWVtYmVyX25vIjoxOTQ1NTg0NjIsImFwcGxpY2F0aW9uX25vIjoxMDAwMn0.eK6eoYXKQN2ZRixuXSSqY1qFi9_uCK6sMyHUcFx9Nyhb2gz1a4KeApt-obgSy2FDMoQ61bdaeeHlGKHs-0EpLU1z6Ivssb8ykIL6N8Wbu0by5eKB_kiPbxwyWHqTJyOkMoEP6zZnxBMtZMTf_rGUkyyQ9nPZBeTXEgPtAoZe_w_AHG2JYVtAiMDfRd_uqfsed9EbxA_RzvJLKPNbSx3T8qomj15O7tZvxo2WNNdmjkbPiKNNUoh4vQSc2Oim68Nr";
-
-            Connection.Response doc = Jsoup.connect(LOA_MARKET_URL)
-                    .cookie("SUAT", jwt)
-                    .data(reqData)
-                    .method(Connection.Method.POST)
+            Connection.Response loginResponse = conn
+                    .timeout(5000)
+                    .method(Connection.Method.GET)
                     .execute();
 
-            System.out.println(doc.parse().html());
+            Map<String, String> cookies = loginResponse.cookies();
+
+            Connection.Response response = Jsoup.connect(LOA_LOGIN_URL)
+                    .data("user_id", userId, "user_pwd", password, "forever", "false")
+                    .method(Connection.Method.POST)
+                    .ignoreContentType(true)
+                    .timeout(5000)
+                    .execute();
+
+            System.out.println(response.parse().html());
+            System.out.println(response.cookies());
+//            HashMap<String, String> reqData = new HashMap<>();
+//            reqData.put("firstCategory", "50000");
+//            reqData.put("secondCategory", "50010");
+//            reqData.put("characterClass", "");
+//            reqData.put("tier", "0");
+//            reqData.put("grade", "99");
+//            reqData.put("itemName", "명예의 파편");
+//            reqData.put("pageNo", "1");
+//            reqData.put("isInit", "false");
+//            reqData.put("sortType", "7");
+//
+//            String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJleHBpcmVfdGltZSI6MTY0NjY4MjIzNjc0MiwibWVtYmVyX25vIjoxOTQ1NTg0NjIsImFwcGxpY2F0aW9uX25vIjoxMDAwMn0.eK6eoYXKQN2ZRixuXSSqY1qFi9_uCK6sMyHUcFx9Nyhb2gz1a4KeApt-obgSy2FDMoQ61bdaeeHlGKHs-0EpLU1z6Ivssb8ykIL6N8Wbu0by5eKB_kiPbxwyWHqTJyOkMoEP6zZnxBMtZMTf_rGUkyyQ9nPZBeTXEgPtAoZe_w_AHG2JYVtAiMDfRd_uqfsed9EbxA_RzvJLKPNbSx3T8qomj15O7tZvxo2WNNdmjkbPiKNNUoh4vQSc2Oim68Nr";
+//
+//            Connection.Response doc = Jsoup.connect(LOA_MARKET_URL)
+//                    .cookie("SUAT", jwt)
+//                    .data(reqData)
+//                    .method(Connection.Method.POST)
+//                    .execute();
+//
+//            System.out.println(doc.parse().html());
         } catch (IOException e) {
             e.printStackTrace();
         }
