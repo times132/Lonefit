bbd56918985c88703a89fd5d858b751cdcc09bc5
times132 <times132@gmail.com>
2022-03-10 22:33:04 +0900

    jsoup html parsing data

--- lonefit-batch/src/main/java/com/times/lonefitbatch/jsoup/LoaDataService.java
+++ lonefit-batch/src/main/java/com/times/lonefitbatch/jsoup/LoaDataService.java
@@ -3,6 +3,8 @@ package com.times.lonefitbatch.jsoup;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +23,7 @@ public class LoaDataService {
     @Value("${loa.password}")
     private String password;
 
-    @PostConstruct
+    //@PostConstruct
     public void getMarket() {
         Connection conn = Jsoup.connect("https://member.onstove.com/auth/login");
 
@@ -31,8 +33,6 @@ public class LoaDataService {
                     .method(Connection.Method.GET)
                     .execute();
 
-            Map<String, String> cookies = loginResponse.cookies();
-
             Connection.Response response = Jsoup.connect(LOA_LOGIN_URL)
                     .data("user_id", userId, "user_pwd", password, "forever", "false")
                     .method(Connection.Method.POST)
@@ -40,28 +40,34 @@ public class LoaDataService {
                     .timeout(5000)
                     .execute();
 
-            System.out.println(response.parse().html());
-            System.out.println(response.cookies());
-//            HashMap<String, String> reqData = new HashMap<>();
-//            reqData.put("firstCategory", "50000");
-//            reqData.put("secondCategory", "50010");
-//            reqData.put("characterClass", "");
-//            reqData.put("tier", "0");
-//            reqData.put("grade", "99");
-//            reqData.put("itemName", "명예의 파편");
-//            reqData.put("pageNo", "1");
-//            reqData.put("isInit", "false");
-//            reqData.put("sortType", "7");
-//
-//            String jwt = "eyJhbGciOiJIUzI1NiJ9.eyJleHBpcmVfdGltZSI6MTY0NjY4MjIzNjc0MiwibWVtYmVyX25vIjoxOTQ1NTg0NjIsImFwcGxpY2F0aW9uX25vIjoxMDAwMn0.eK6eoYXKQN2ZRixuXSSqY1qFi9_uCK6sMyHUcFx9Nyhb2gz1a4KeApt-obgSy2FDMoQ61bdaeeHlGKHs-0EpLU1z6Ivssb8ykIL6N8Wbu0by5eKB_kiPbxwyWHqTJyOkMoEP6zZnxBMtZMTf_rGUkyyQ9nPZBeTXEgPtAoZe_w_AHG2JYVtAiMDfRd_uqfsed9EbxA_RzvJLKPNbSx3T8qomj15O7tZvxo2WNNdmjkbPiKNNUoh4vQSc2Oim68Nr";
-//
-//            Connection.Response doc = Jsoup.connect(LOA_MARKET_URL)
-//                    .cookie("SUAT", jwt)
-//                    .data(reqData)
-//                    .method(Connection.Method.POST)
-//                    .execute();
-//
-//            System.out.println(doc.parse().html());
+            HashMap<String, String> reqData = new HashMap<>();
+            reqData.put("firstCategory", "50000");
+            reqData.put("secondCategory", "50010");
+            reqData.put("characterClass", "");
+            reqData.put("tier", "0");
+            reqData.put("grade", "99");
+            reqData.put("itemName", "명예의 파편");
+            reqData.put("pageNo", "1");
+            reqData.put("isInit", "false");
+            reqData.put("sortType", "7");
+
+            Document doc = Jsoup.connect(LOA_MARKET_URL)
+                    .cookies(response.cookies())
+                    .data(reqData)
+                    .post();
+
+            //System.out.println(doc);
+
+            Element itemElement = doc.getElementById("tbodyItemList");
+            String item1Name = itemElement.getElementsByClass("name").get(0).text();
+            String itemPrice1 = itemElement.getElementsByTag("em").get(2).text();
+            String item2Name = itemElement.getElementsByClass("name").get(1).text();
+            String itemPrice2 = itemElement.getElementsByTag("em").get(5).text();
+            String item3Name = itemElement.getElementsByClass("name").get(2).text();
+            String itemPrice3 = itemElement.getElementsByTag("em").get(8).text();
+            System.out.println(item1Name + " : " + itemPrice1);
+            System.out.println(item2Name + " : " + itemPrice2);
+            System.out.println(item3Name + " : " + itemPrice3);
         } catch (IOException e) {
             e.printStackTrace();
         }
